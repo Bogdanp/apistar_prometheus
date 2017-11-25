39228785461d192cc5e54f6da23e31955e584fdd
Bogdan Popa <popa.bogdanp@gmail.com>
2017-11-25 18:58:17 +0200

    refactor: rename endpoints to handlers

--- apistar_prometheus/__init__.py
+++ apistar_prometheus/__init__.py
@@ -1,5 +1,5 @@
 from .components import Prometheus
-from .endpoints import expose_metrics, expose_metrics_multiprocess
+from .handlers import expose_metrics, expose_metrics_multiprocess
 from .hooks import before_request, after_request
 
 __version__ = "0.2.2"
