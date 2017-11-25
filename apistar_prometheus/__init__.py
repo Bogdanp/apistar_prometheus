1510be215556a76e59648f16aa3c2f8425f25292
Bogdan Popa <popa.bogdanp@gmail.com>
2017-11-25 18:58:20 +0200

    chore: bump version 0.2.2 → 0.3.0

--- apistar_prometheus/__init__.py
+++ apistar_prometheus/__init__.py
@@ -2,7 +2,7 @@ from .components import Prometheus
 from .handlers import expose_metrics, expose_metrics_multiprocess
 from .hooks import before_request, after_request
 
-__version__ = "0.2.2"
+__version__ = "0.3.0"
 __all__ = [
     "Prometheus",
     "before_request", "after_request",
